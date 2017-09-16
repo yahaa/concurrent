1f93a41e6fc2ac1e8c2d7ce18c73541f455d5dde
yzihua <yuanzihua0@gmail.com>
2017-09-16 23:34:27 +0800

    基于线程池实现的简单http服务器

---
+++ src/main/java/simpleHttpServer/SimpleHttpServer.java
@@ -0,0 +1,108 @@
+package simpleHttpServer;
+
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SimpleHttpServer {
+    static ExecutorService threadPool = Executors.newCachedThreadPool();
+    static String basePath;
+    static ServerSocket serverSocket;
+    static int port = 8080;
+
+    public static void setPort(int port) {
+        if (port > 0) {
+            SimpleHttpServer.port = port;
+        }
+    }
+
+    public static void setBasePath(String basePath) {
+        if (basePath != null && new File(basePath).exists() && new File(basePath).isDirectory()) {
+            SimpleHttpServer.basePath = basePath;
+            System.out.println("初始化web服务器路径");
+        }
+    }
+
+    public static void start() throws Exception {
+        System.out.println("listen on http://127.0.0.1:8080");
+        serverSocket = new ServerSocket(port);
+        Socket socket = null;
+        while ((socket = serverSocket.accept()) != null) {
+            System.out.println("接入客户端");
+            threadPool.execute(new HttpRequestHandler(socket));
+        }
+        serverSocket.close();
+    }
+
+    static class HttpRequestHandler implements Runnable {
+        private Socket socket;
+
+        public HttpRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+        public void run() {
+            String line = null;
+            BufferedReader br = null;
+            BufferedReader reader = null;
+            PrintWriter out = null;
+            InputStream in = null;
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = reader.readLine();
+                String filePath = basePath + header.split(" ")[1];
+                System.out.println(filePath);
+                out = new PrintWriter(socket.getOutputStream());
+                if (filePath.endsWith("jpg") || filePath.endsWith("png")) {
+                    System.out.println("open image");
+                    in = new FileInputStream(filePath);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i = 0;
+                    while ((i = in.read()) != -1) {
+                        baos.write(i);
+                    }
+                    byte[] array = baos.toByteArray();
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server: Molly");
+                    out.println("Content-Type: image/png");
+                    out.println("Content-Length: " + array.length);
+                    out.println("");
+                    socket.getOutputStream().write(array, 0, array.length);
+                } else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+                    out = new PrintWriter(socket.getOutputStream());
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server: Molly");
+                    out.println("Content-Type: text/html; charset=UTF-8");
+                    out.println("");
+                    while ((line = br.readLine()) != null) {
+                        out.println(line);
+                    }
+                }
+                out.flush();
+
+            } catch (Exception e) {
+                out.println("HTTP/1.1 500");
+                out.println("");
+                out.flush();
+            }finally {
+                close(br,in,reader,out,socket);
+            }
+        }
+
+        private static void close(Closeable... closeables) {
+            if (closeables != null) {
+                for (Closeable closeable : closeables) {
+                    try {
+                        closeable.close();
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        }
+    }
+}
